150b85034e13b42b7af2317a61204d51769753ce
tonothan <antoniakaraisl@gmail.com>
2025-03-28 12:49:22 +0000

    Deploying to gh-pages from @ tonothan/sangaku-archive@209317360defc96f65f78e422ec74a092c8dee2a 🚀

--- _next/static/chunks/839.a5ef0709c76fb3a8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[839],{7690:function(s){s.exports=JSON.parse('[{"path":"/essays","text":"Contemporary Solutions"},{"path":"/essays/mishima-sangaku","text":"神奈川　三島神社算額第四問"}]')}}]);
